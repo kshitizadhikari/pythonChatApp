8a7a7c695ebce97eda853243f892cf3cc4ae9bd4
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2024-05-23 12:07:46 +0545

    add server functionality

--- client.py
+++ client.py
@@ -10,12 +10,16 @@ import threading
 ServerHOST = '127.0.0.1'
 ServerPort = 1234
 
+
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
         client.connect((ServerHOST, ServerPort))
         print("Successfully connected to server")
+
+
+
     except:
         print(f"Unable to connect to ServerHost: {ServerHOST}, ServerPort: {ServerPort}")
     
--- server.py
+++ server.py
@@ -10,6 +10,41 @@ accept connection when the client sends connection request
 HOST = '127.0.0.1'
 PORT = 1234
 LISTENER_LIMIT = 5
+active_clients = []
+
+
+def listen_for_message(client, username):
+    while 1:
+        message = client.recv(2048).decode('utf-8')
+        # if message == '':
+        #     print(f"Message sent by {username} is empty")
+        
+        final_msg = f"{username}: {message}"
+        send_message_to_all(final_msg)
+
+
+#send message to all
+def send_message_to_all(message):
+    for client in active_clients:
+        send_message(client[1], message)
+
+#send message to one person
+def send_message(client, message):
+    client.sendall(message.encode())
+
+#client handling function
+def client_handler(client: socket):
+    while 1:
+        username = client.recv(1024).decode('utf-8')
+        if username == '':
+            raise Exception("Username empty")
+        break
+        
+    active_clients.append((username, client))
+
+    threading.Thread(target=listen_for_message, args=(client, username, )).start()
+        
+
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,8 +59,9 @@ def main():
 
     while 1:
         client, address = server.accept()
+        print(f"Connected to client, Client Host: {address[0]}, Client Port: {address[1]}")
 
-        print(f"Connected to client: {client}, Client Host: {address[0]}, Client Port: {address[1]}")
+        threading.Thread(target=client_handler, args=(client, )).start()
 
 
 
